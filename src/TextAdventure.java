37ae9cab89351bc054cc3e39361572d2f33deb31
Nathaniel Bustos <nbustos@mtu.edu>
2026-04-12 17:46:46 -0400

    Refactor main method to start game with Game class

--- src/TextAdventure.java
+++ src/TextAdventure.java
@@ -1,34 +1,14 @@
-//Nate Bustos
-
-import java.util.Scanner;
+// Author: Nate Bustos
 
 public class TextAdventure {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Welcome to the Text Adventure Game!");
-        System.out.println("Type commands like GO, LOOK, GET, DROP, INVENTORY, QUIT");
+        System.out.println("=== TEXT ADVENTURE GAME ===");
+        System.out.println("Starting game...");
         System.out.println();
 
-        // Temporary simple loop until Game.java is finished
-        boolean running = true;
-
-        while (running) {
-            System.out.print("> ");
-            String input = scanner.nextLine();
-
-            // Basic command handling (temporary)
-            if (input.equalsIgnoreCase("quit")) {
-                System.out.println("Goodbye!");
-                running = false;
-            } else {
-                System.out.println("You typed: " + input);
-                System.out.println("(Game logic not connected yet)");
-            }
-        }
-
-        scanner.close();
+        Game game = new Game();
+        game.startGame();
     }
 }
